fc9fd95214274dbf8881fbc1ab2cd862e489fd5d
Data Magic <66531576+datamagic2020@users.noreply.github.com>
2022-02-03 11:20:16 +0530

    initial commit

---
+++ main-app.py
@@ -0,0 +1,29 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('weight_pred_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/getprediction',methods=['POST'])
+def getprediction():    
+
+    input = [float(x) for x in request.form.values()]
+    final_input = [np.array(input)]
+    prediction = model.predict(final_input)
+
+    return render_template('index.html', output='Predicted Weight in KGs :{}'.format(prediction))
+   
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+    #app.run(debug=True)
+    
+    
+    
+
+    
